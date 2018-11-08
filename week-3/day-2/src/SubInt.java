9e4d2be105a5ee4b8db7358ff10c7feb1f6754b2
Zsolt Jenofi <jenofi.zsolt@gmail.com>
2018-11-08 12:38:55 +0100

    SubInt upgrade

--- week-3/day-2/src/SubInt.java
+++ week-3/day-2/src/SubInt.java
@@ -5,15 +5,25 @@ import java.util.List;
 public class SubInt {
     public static void main(String[] args) {
 
-        System.out.println(subInt(5, Arrays.asList(13, 25, 3, 4, 43, 47, 73)));
-
+        System.out.println(subInt(43, Arrays.asList(13, 1200, 125, 4, 43, 47, 12001)));
     }
 
     public static ArrayList<Integer> subInt(int sub, List<Integer> listIni) {
         ArrayList<Integer> listIndex = new ArrayList<>();
+        int divider = 10;
+        String subStr = Integer.toString(sub);
+        String dividerStr = Integer.toString(divider);
+
+        if (subStr.length() > 1) {
+            for (int i = 0; i < subStr.length() - 1; i++) {
+                dividerStr += "0";
+            }
+        }
+        int dividerInt = Integer.parseInt(dividerStr);
+        int subInt = Integer.parseInt(subStr);
 
         for (int i = 0; i < listIni.size(); i++) {
-            if (listIni.get(i) % 10 == sub) {
+            if (listIni.get(i) % dividerInt == sub) {
                 listIndex.add(i + 1);
             }
         }
