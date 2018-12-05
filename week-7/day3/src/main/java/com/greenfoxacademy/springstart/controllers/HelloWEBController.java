e39e66c8d90a5896cdf2e80192217bfb9852a851
Zsolt Jenofi <jenofi.zsolt@gmail.com>
2018-12-05 14:50:54 +0100

    name query and counter added

--- week-7/day3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWEBController.java
+++ week-7/day3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWEBController.java
@@ -1,15 +1,18 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWEBController {
 
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", "Zsolt");
+    public String greeting(@RequestParam("name") String name, Model model, Greeting greeting) {
+        model.addAttribute("name", name);
+        model.addAttribute("counter", greeting.getId());
         return "greeting";
     }
 }
