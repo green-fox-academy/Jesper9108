d42e7f9f022d47265b72b4c59cb26842bccd55a9
Zsolt Jenofi <jenofi.zsolt@gmail.com>
2018-12-13 14:08:16 +0100

    base fox application

---
+++ week-8/day5/src/main/java/com/foxclub/foxstart/FoxstartApplication.java
@@ -0,0 +1,12 @@
+package com.foxclub.foxstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxstartApplication.class, args);
+    }
+}
---
+++ week-8/day5/src/test/java/com/foxclub/foxstart/FoxstartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foxclub.foxstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoxstartApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
